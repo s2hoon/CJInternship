e95eec8c29e52d2e570ba1023ac9aeef490408a2
s2hoon <jobak4200nim@nate.com>
2025-07-14 17:14:35 +0900

    feat : application.yaml

--- src/main/java/com/cj/cjone/CjoneApplication.java
+++ src/main/java/com/cj/cjone/CjoneApplication.java
@@ -2,11 +2,9 @@ package com.cj.cjone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // ci 테스트
 @SpringBootApplication
-@EnableFeignClients // Feign Client를 사용하도록 설정
 public class CjoneApplication {
 
     public static void main(String[] args) {
